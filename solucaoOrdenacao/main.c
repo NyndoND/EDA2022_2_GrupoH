5bd7fafce248b1eeee38722b1e4b8acd720922b8
renann.gomes <renannpop11@gmail.com>
2023-01-16 19:48:35 -0300

    Melhorar a interacao de sim e não

--- solucaoOrdenacao/main.c
+++ solucaoOrdenacao/main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-//Definindo o Produto
+// Definindo o Produto
 typedef struct
 {
   char category[30];
@@ -18,9 +18,20 @@ int main()
   clock_t start, end;
   double cpu_time_used;
 
-  start = clock(); //Pegando o tempo de inicio do programa
+  char input[5];
+  do
+  { 
+    printf("Seu Dataset possui espaços entre as categorias? (s/n)\n");
+    scanf("%s", input);
+    if (input != "s" && input != "n")
+    {
+      printf("Resposta invalida!\n");
+    }
+  } while (input != "s" || input != "n");
 
-  fp = fopen("amazonDataset.csv", "r"); //Abrindo o arquivo para leitura seguido de verificação de erro
+  start = clock(); // Pegando o tempo de inicio do programa
+
+  fp = fopen("amazonTest.csv", "r"); // Abrindo o arquivo para leitura seguido de verificação de erro
 
   if (fp == NULL)
   {
@@ -33,16 +44,38 @@ int main()
 
   p = (Produto *)malloc(1 * sizeof(Produto)); // Alocando o tamanho de 1 produto
 
-  
   fpCopia = fopen("amazonDatasetCopia.csv", "w"); // Abrindo o outro arquivo para Saída
 
   int i = 0; // Contador para manipular a posição no Vetor
-  while(fscanf(fp, "%s %f", aux.category, &aux.amount) != EOF){
-    p = (Produto*) realloc(p, (i+1) * sizeof(Produto));
-    p[i].amount = aux.amount;
-    strcpy(p[i].category,aux.category);
-    i++;
-  };
+  if (input == "s")
+  {
+    while (fscanf(fp, "%s %f", aux.category, &aux.amount) != EOF)
+    {
+      p = (Produto *)realloc(p, (i + 1) * sizeof(Produto));
+      p[i].amount = aux.amount;
+      strcpy(p[i].category, aux.category);
+      i++;
+    };
+  }
+  else
+  {
+    while (fscanf(fp, "%s", aux.category) != EOF)
+    {
+      char *category = strtok(aux.category, ";"); // Pegando a categoria
+      strcpy(aux.category, category);
+
+      char *amount = strtok(NULL, ";"); // Pegando o preço
+      aux.amount = strtod(amount, NULL);
+
+      // adicionando o ; no final da categoria
+      strcat(aux.category, ";");
+      // colocando no vetor p
+      p = (Produto *)realloc(p, (i + 1) * sizeof(Produto));
+      p[i].amount = aux.amount;
+      strcpy(p[i].category, aux.category);
+      i++;
+    };
+  }
 
   // Ordenado os precos usando lógica BubbleSort
   for (int h = i; h > 0; h--)
@@ -63,7 +96,7 @@ int main()
     }
   }
 
-  //Escrevendo no arquivo de Saída
+  // Escrevendo no arquivo de Saída
   for (int l = 0; l < i; l++)
   {
     fprintf(fpCopia, "%s %f\n", p[l].category, p[l].amount);
@@ -73,8 +106,8 @@ int main()
   free(p);
   fclose(fp);
 
-  end = clock(); //Pegando o tempo de fim do programa
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  end = clock(); // Pegando o tempo de fim do programa
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
   fprintf(fpCopia, "O tempo de execução foi de: %f segundos\n", cpu_time_used); // Imprimindo na última linha o tempo de execução
   fclose(fpCopia);
   return 0;
